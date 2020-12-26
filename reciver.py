1a37c44dba9a832b16e92af756c0c556c6c1b100
pujanrajrai <pujanr0809@gmail.com>
2020-12-26 17:15:23 +0545

    reciver

---
+++ reciver.py
@@ -0,0 +1,28 @@
+from scapy.all import Raw, sniff
+from scapy.layers.inet import IP, TCP
+
+
+def analyze(packet):
+    """ Analyze the sniffed packets. """
+    try:
+        # Read only TCP packet if packet.haslayer(TCP):
+        # Read only reply from sender PC in given port number
+        if packet[IP].src == ip_address and packet[TCP].dport == int(port):
+            print('Source:', packet[IP].src)
+            print('Message:', packet[Raw].load.decode('utf-8'))
+            print('-' * 100)
+    except:
+        pass
+
+
+# Parameters
+print('-' * 100)
+interface = input("Enter interface: ")
+ip_address = input("Enter IP address to listen: ")
+port = input("Enter port to listen: ")
+print('-' * 100)
+# Status
+print("Listening on interface '{}' in port {} for '{}'...".format(interface, port, ip_address))
+print('-' * 100)
+# Sniff
+sniffed = sniff(iface=interface, store=False, prn=analyze)
